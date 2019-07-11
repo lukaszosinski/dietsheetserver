ab245d49e206d379a7b39adb1f91c3c6e1838d40
lukaszosinski <nuras9988@gmail.com>
2019-07-11 19:58:39 +0200

    CORS configuration v2

--- src/main/java/com/dietsheet_server/DietsheetServerApplication.java
+++ src/main/java/com/dietsheet_server/DietsheetServerApplication.java
@@ -9,12 +9,6 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 
 @SpringBootApplication(exclude = {
@@ -29,22 +23,6 @@ public class DietsheetServerApplication {
     @Bean
     public Module datatypeHibernateModule() {
         return new Hibernate5Module();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.setAllowedMethods(Arrays.asList(
-                HttpMethod.GET.name(),
-                HttpMethod.HEAD.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name()));
-        corsConfiguration.setMaxAge(1800L);
-        source.registerCorsConfiguration("/**", corsConfiguration); // you restrict your path here
-        return source;
     }
 
 }
